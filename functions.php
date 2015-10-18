a18ac2cab35e893841fde7a8d495ae443bbefa5f
Murray Anderson-Clemence <me@murray.ac>
2015-10-18 21:00:02 +1000

    Create functions.php
    Add this code to line 50 of functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+//* Add support for custom header
+add_theme_support( 'custom-header', array(
+	'flex-height'     => true,
+	'width'           => 400,
+	'height'          => 80,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+) );
